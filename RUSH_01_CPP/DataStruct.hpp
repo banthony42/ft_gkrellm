22131aaf50d9e3dfdaeb4680ff74667f4e2cd3b7
Maxime GRAS <mgras@e1r6p12.42.fr>
2018-01-20 18:13:11 +0100

    Addition de SIZET dans l'enum

--- RUSH_01_CPP/DataStruct.hpp
+++ RUSH_01_CPP/DataStruct.hpp
@@ -6,7 +6,7 @@
 /*   By: mgras <mgras@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/20 12:40:28 by vnoon             #+#    #+#             */
-/*   Updated: 2018/01/20 18:11:15 by mgras            ###   ########.fr       */
+/*   Updated: 2018/01/20 18:12:58 by mgras            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 #include <cstddef>
 
-enum    E_VarType {UNKNOWN_TYPE, CHAR, CHAR_PTR, UCHAR, INT, UINT, LONGINT, ULONGINT, DOUBLE, TIME_T, TM, UINT64, UINT32};
+enum    E_VarType {UNKNOWN_TYPE, CHAR, CHAR_PTR, UCHAR, INT, UINT, LONGINT, ULONGINT, DOUBLE, TIME_T, TM, UINT64, UINT32, SIZET};
 enum    E_DisplayType {UNKNOWN_DISPLAY, STRING, VALUE, GRAPH};
 
 class       DataStruct {
